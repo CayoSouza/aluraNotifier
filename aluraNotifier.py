3b8fa54bce92dea3325be2e5220a1f4248c6ca1f
cayosouza <cayomps@gmail.com>
2018-03-06 15:51:10 -0300

    add logs e feedbacks no console, terminacao automatica caso nao haja secoes para verificar

--- aluraNotifier.py
+++ aluraNotifier.py
@@ -13,10 +13,10 @@ SECTIONS_TO_VERIFY = {'JAVA':'https://cursos.alura.com.br/forum/subcategoria-jav
                       'GERAL':'https://cursos.alura.com.br/forum/sem-resposta/'}
 
 def checkForDoubtsWithOutAnswer(seconds):
-    while True:
+    while ( len(SECTIONS_TO_VERIFY) > 0 ):
         for secao, url in SECTIONS_TO_VERIFY.copy().items():
             result = urllib.request.urlopen(url)
-            print('Verificando: '+url)
+            print('>>Verificando '+ secao +'<<')
 
             soup = BeautifulSoup(result, 'html.parser')
 
@@ -30,22 +30,32 @@ def checkForDoubtsWithOutAnswer(seconds):
                     itemIds += "\u00BB"+m.groups()[0] + "\n"
 
                 ret_value = ctypes.windll.user32.MessageBoxW(0, "Existe(m) " + str(len(forumListItems)) +" dúvida(s) "
-                                                             + "sem resposta na seção "+secao+".\n\n"
+                                                             + "sem resposta na seção "+ secao +".\n\n"
                                                              + itemIds
                                                              + "\n\nGostaria de abrir o link?"
                                                              , "Alura Notifier", 3)
+                print('Foram encontradas %s dúvidas em %s' % (len(forumListItems), secao))
+                
                 # 6 = YES
                 if (ret_value == 6):
-                    webbrowser.open(r"https://cursos.alura.com.br"+str(forumListItems[0].get('itemid'))) if ( (len(forumListItems)) == 1 ) else webbrowser.open(url)
+                    webbrowser.open(r"https://cursos.alura.com.br"+ str(forumListItems[0].get('itemid'))) if ( (len(forumListItems)) == 1 ) else webbrowser.open(url)
+                    print('=>Seção %s acessada' % secao)
                 # 2 = CANCEL or EXIT
                 elif (ret_value == 2):
-                    sys.exit(0)
+                    finalizarPrograma('Cancel clicado, terminando programa.')
                 # NO
                 else:
                     SECTIONS_TO_VERIFY.pop(secao, None)
-                    print('Próxima verificação: '+str(SECTIONS_TO_VERIFY))
+                    print('->%s foi removido da lista de monitoramento' % secao)
+                    print('Próxima verificação: %s' % SECTIONS_TO_VERIFY.keys())
+            else:
+                print('Nenhuma dúvida sem resposta na seção %s' % secao) 
+        print('Esperando %s segundos para executar a próxima busca...\n' % seconds)        
+        time.sleep(seconds)
+    finalizarPrograma('Nada para verificar, terminando programa.')
 
-                
-    time.sleep(seconds)
+def finalizarPrograma(msg):
+    print(msg)
+    sys.exit(0)
 
-checkForDoubtsWithOutAnswer(10)
+checkForDoubtsWithOutAnswer(5)
