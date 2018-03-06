e9d3991a0755dc6f31ec7e2d5d8356839ad9c4e2
cayosouza <cayomps@gmail.com>
2018-03-06 17:48:51 -0300

    add feature de só avisar novamente quando houve a criacao de um topico novo

--- aluraNotifier.py
+++ aluraNotifier.py
@@ -6,13 +6,22 @@ import webbrowser
 import sys
 import re
 
-SECTIONS_TO_VERIFY = {'JAVA':'https://cursos.alura.com.br/forum/subcategoria-java/sem-resposta/',
-                      'JAVASCRIPT':'https://cursos.alura.com.br/forum/subcategoria-javascript/sem-resposta/',
-                      'JQUERY':'https://cursos.alura.com.br/forum/subcategoria-jquery/sem-resposta/',
-                      'SQL':'https://cursos.alura.com.br/forum/subcategoria-sql/sem-resposta/',
-                      'GERAL':'https://cursos.alura.com.br/forum/sem-resposta/'}
+SECTIONS_TO_VERIFY = {#'JAVA':'https://cursos.alura.com.br/forum/subcategoria-java/sem-resposta/',
+                      #'JAVASCRIPT':'https://cursos.alura.com.br/forum/subcategoria-javascript/sem-resposta/',
+                      #'JQUERY':'https://cursos.alura.com.br/forum/subcategoria-jquery/sem-resposta/',
+                      #'SQL':'https://cursos.alura.com.br/forum/subcategoria-sql/sem-resposta/',
+                      #'GERAL':'https://cursos.alura.com.br/forum/sem-resposta/',
+                      'FRONT-END':'https://cursos.alura.com.br/forum/categoria-front-end/sem-resposta/',
+                      'OFF-TOPIC':'https://cursos.alura.com.br/forum/categoria-offtopic/sem-resposta/',
+                      'INFRAESTRUTURA':'https://cursos.alura.com.br/forum/categoria-infraestrutura/sem-resposta/',
+                      'PROGRAMACAO':'https://cursos.alura.com.br/forum/categoria-programacao/sem-resposta/',
+                      'MOBILE':'https://cursos.alura.com.br/forum/categoria-mobile/sem-resposta/',
+                      'DESIGN & UX':'https://cursos.alura.com.br/forum/categoria-design-ux/sem-resposta/',
+                      'BUSINESS':'https://cursos.alura.com.br/forum/categoria-business/sem-resposta/'}
 
 def checkForDoubtsWithOutAnswer(seconds):
+    topicosVistos = []
+    topicoNovo = True
     while ( len(SECTIONS_TO_VERIFY) > 0 ):
         for secao, url in SECTIONS_TO_VERIFY.copy().items():
             result = urllib.request.urlopen(url)
@@ -26,9 +35,19 @@ def checkForDoubtsWithOutAnswer(seconds):
                 forumListItems = soup.find_all('li', attrs={'class':'forumList-item'})
                 itemIds = ''
                 for item in forumListItems:
-                    m = re.search("-([a-zA-z-]+)-", item.get('itemid'))
-                    itemIds += "\u00BB"+m.groups()[0] + "\n"
+                    match = re.search("-([a-zA-z-]+)-", item.get('itemid')).groups()[0]
+                    itemIds += "\u00BB"+ match + "\n"
 
+                    if match not in topicosVistos:
+                        topicosVistos.append(match)
+                        topicoNovo = True
+
+                if (topicoNovo == False):
+                    continue
+                
+                topicoNovo = False
+                
+                print(topicosVistos)
                 ret_value = ctypes.windll.user32.MessageBoxW(0, "Existe(m) " + str(len(forumListItems)) +" dúvida(s) "
                                                              + "sem resposta na seção "+ secao +".\n\n"
                                                              + itemIds
@@ -45,11 +64,12 @@ def checkForDoubtsWithOutAnswer(seconds):
                     finalizarPrograma('Cancel clicado, terminando programa.')
                 # NO
                 else:
-                    SECTIONS_TO_VERIFY.pop(secao, None)
-                    print('->%s foi removido da lista de monitoramento' % secao)
+                    #SECTIONS_TO_VERIFY.pop(secao, None)
+                    #print('->%s foi removido da lista de monitoramento' % secao)
                     print('Próxima verificação: %s' % SECTIONS_TO_VERIFY.keys())
             else:
-                print('Nenhuma dúvida sem resposta na seção %s' % secao) 
+                print('Nenhuma dúvida sem resposta na seção %s' % secao)
+        
         print('Esperando %s segundos para executar a próxima busca...\n' % seconds)        
         time.sleep(seconds)
     finalizarPrograma('Nada para verificar, terminando programa.')
