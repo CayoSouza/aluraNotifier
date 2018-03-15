54743e14581e4609c9df449ca57b2b84c73671e9
cayosouza <cayomps@gmail.com>
2018-03-15 13:01:55 -0300

    ultima versao com pequenas alteracoes

--- aluraNotifier.py
+++ aluraNotifier.py
@@ -6,18 +6,21 @@ import webbrowser
 import sys
 import re
 
-SECTIONS_TO_VERIFY = {#'JAVA':'https://cursos.alura.com.br/forum/subcategoria-java/sem-resposta/',
-                      #'JAVASCRIPT':'https://cursos.alura.com.br/forum/subcategoria-javascript/sem-resposta/',
-                      #'JQUERY':'https://cursos.alura.com.br/forum/subcategoria-jquery/sem-resposta/',
-                      #'SQL':'https://cursos.alura.com.br/forum/subcategoria-sql/sem-resposta/',
+
+## Edite este dicionario para customizar as secoes a serem monitoradas ##
+SECTIONS_TO_VERIFY = {'JAVA':'https://cursos.alura.com.br/forum/subcategoria-java/sem-resposta/',
+                      'JAVASCRIPT':'https://cursos.alura.com.br/forum/subcategoria-javascript/sem-resposta/',
+                      'JQUERY':'https://cursos.alura.com.br/forum/subcategoria-jquery/sem-resposta/',
+                      'SQL':'https://cursos.alura.com.br/forum/subcategoria-sql/sem-resposta/',
                       #'GERAL':'https://cursos.alura.com.br/forum/sem-resposta/',
-                      'FRONT-END':'https://cursos.alura.com.br/forum/categoria-front-end/sem-resposta/',
-                      'OFF-TOPIC':'https://cursos.alura.com.br/forum/categoria-offtopic/sem-resposta/',
-                      'INFRAESTRUTURA':'https://cursos.alura.com.br/forum/categoria-infraestrutura/sem-resposta/',
-                      'PROGRAMACAO':'https://cursos.alura.com.br/forum/categoria-programacao/sem-resposta/',
-                      'MOBILE':'https://cursos.alura.com.br/forum/categoria-mobile/sem-resposta/',
-                      'DESIGN & UX':'https://cursos.alura.com.br/forum/categoria-design-ux/sem-resposta/',
-                      'BUSINESS':'https://cursos.alura.com.br/forum/categoria-business/sem-resposta/'}
+                      #'FRONT-END':'https://cursos.alura.com.br/forum/categoria-front-end/sem-resposta/',
+                      #'OFF-TOPIC':'https://cursos.alura.com.br/forum/categoria-offtopic/sem-resposta/',
+                      #'INFRAESTRUTURA':'https://cursos.alura.com.br/forum/categoria-infraestrutura/sem-resposta/',
+                      #'PROGRAMACAO':'https://cursos.alura.com.br/forum/categoria-programacao/sem-resposta/',
+                      #'MOBILE':'https://cursos.alura.com.br/forum/categoria-mobile/sem-resposta/',
+                      #'DESIGN & UX':'https://cursos.alura.com.br/forum/categoria-design-ux/sem-resposta/',
+                      #'BUSINESS':'https://cursos.alura.com.br/forum/categoria-business/sem-resposta/'
+                      }
 
 def checkForDoubtsWithOutAnswer(seconds):
     topicosVistos = []
@@ -25,13 +28,13 @@ def checkForDoubtsWithOutAnswer(seconds):
     while ( len(SECTIONS_TO_VERIFY) > 0 ):
         for secao, url in SECTIONS_TO_VERIFY.copy().items():
             result = urllib.request.urlopen(url)
-            print('>>Verificando '+ secao +'<<')
+            print('>>Verificando %s<<' % secao)
 
             soup = BeautifulSoup(result, 'html.parser')
 
-            alert_message = soup.find('p', attrs={'class':'alert-message'})
+            no_threads_alert_message = soup.find('p', attrs={'class':'alert-message'})
 
-            if not(alert_message):
+            if not(no_threads_alert_message):
                 forumListItems = soup.find_all('li', attrs={'class':'forumList-item'})
                 itemIds = ''
                 for item in forumListItems:
@@ -42,18 +45,18 @@ def checkForDoubtsWithOutAnswer(seconds):
                         topicosVistos.append(match)
                         topicoNovo = True
 
-                if (topicoNovo == False):
+                if not(topicoNovo):
+                    print('Nenhum topico novo')
                     continue
                 
                 topicoNovo = False
                 
-                print(topicosVistos)
                 ret_value = ctypes.windll.user32.MessageBoxW(0, "Existe(m) " + str(len(forumListItems)) +" dúvida(s) "
                                                              + "sem resposta na seção "+ secao +".\n\n"
                                                              + itemIds
                                                              + "\n\nGostaria de abrir o link?"
                                                              , "Alura Notifier", 3)
-                print('Foram encontradas %s dúvidas em %s' % (len(forumListItems), secao))
+                print('%s dúvida(s) em %s' % (len(forumListItems), secao))
                 
                 # 6 = YES
                 if (ret_value == 6):
@@ -63,10 +66,8 @@ def checkForDoubtsWithOutAnswer(seconds):
                 elif (ret_value == 2):
                     finalizarPrograma('Cancel clicado, terminando programa.')
                 # NO
-                else:
-                    #SECTIONS_TO_VERIFY.pop(secao, None)
-                    #print('->%s foi removido da lista de monitoramento' % secao)
-                    print('Próxima verificação: %s' % SECTIONS_TO_VERIFY.keys())
+                #else:
+                #    print('Próxima verificação: %s' % SECTIONS_TO_VERIFY.keys())
             else:
                 print('Nenhuma dúvida sem resposta na seção %s' % secao)
         
@@ -78,4 +79,4 @@ def finalizarPrograma(msg):
     print(msg)
     sys.exit(0)
 
-checkForDoubtsWithOutAnswer(5)
+checkForDoubtsWithOutAnswer(60)
